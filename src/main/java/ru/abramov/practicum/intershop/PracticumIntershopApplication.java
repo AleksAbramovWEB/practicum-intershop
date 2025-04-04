160bb48445eb1bf6ea650d7fee9848f4bf6a0729
Aleksandr Abramov <AleksAbramovWEB@yandex.ru>
2025-04-04 17:00:41 +0300

    Initial commit

---
+++ src/main/java/ru/abramov/practicum/intershop/PracticumIntershopApplication.java
@@ -0,0 +1,13 @@
+package ru.abramov.practicum.intershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticumIntershopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticumIntershopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/abramov/practicum/intershop/PracticumIntershopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.abramov.practicum.intershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticumIntershopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
